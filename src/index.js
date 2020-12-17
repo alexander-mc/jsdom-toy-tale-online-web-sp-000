068ef3e817a33a351f1374476c1707b8340368b4
Alexander M. Clamor <alexander.clamor@gmail.com>
2020-12-17 15:16:59 -0800

    commit tree 08f1c46382a424260943d6648a8a118fd72d0b2c

--- src/index.js
+++ src/index.js
@@ -12,4 +12,28 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  loadToys();
 });
+
+function loadToys() {
+  console.log('hi')
+  // fetch('http://localhost:3000/toys')
+  //   .then(function(response) {
+  //     response.json();
+  //   })
+  //   .then(function(json) {
+  //     console.log(json.message);
+      // const toysContainer = document.getElementById('toy-collection')
+      
+      // for (const el of json) {
+      //   let newCard = document.createElement('div')
+      //   let h2 = document.createElement('h2')
+      //   newCard.className = "card"
+      //   toysContainer.appendChild(newCard)
+      //   h2.innerHTML = el.name
+      //   newCard.appendChild = h2
+      // }
+      // 
+    // })
+}
